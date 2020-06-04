d2785d792a9e1484d6e755b0097d600be86466fb
ysungju25 <60795265+ysungju25@users.noreply.github.com>
2020-06-04 14:43:49 -0400

    goes to tinder dot com and locates google log in button

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 
-class TinderBot():
+class TinderBot(): # creation of tinder bot
         def __init__(self):
             self.driver = webdriver.Chrome()
 
-  
+        def login(self):
+            self.driver.get('https://tinder.com/') # goes to tinder dot com
+
+            self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/div[1]/div/button') # click Google log in button
